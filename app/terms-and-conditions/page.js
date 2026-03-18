6bc9402240c851607f5b5202d30f8aaefdf6d1f9
jahidul hasan <hasanrabbie@gmail.com>
2026-03-18 11:18:42 +0600

    term

--- app/terms-and-conditions/page.js
+++ app/terms-and-conditions/page.js
@@ -7,7 +7,7 @@ import FancyTermsConditions from "../components/spin/FancyTermsConditions";
 export default function TermsAndConditionsPage() {
   return (
     <>
-      <AnimatedSection title="" imageSrc='/assets/terms-condition/terms & condition.png' imageAlt="lucky spin" />
+      <AnimatedSection title="" imageSrc='/assets/terms-condition/terms-and-condition.png' imageAlt="terms and conditions" />
       <AnimatedSectionWrapper animation="fadeInUp" delay={0.14} viewportAmount={0.2}>
         <div className="flex justify-center px-4 py-4 mb-20">
           <FancyTermsConditions />
